b57ea18c2ebc0699c4b9672d731258748be55ea6
Kerton marinho <kerton.marinho@outlook.com>
2025-06-29 11:44:19 -0300

    finalizado tela de login

--- assets/script.js
+++ assets/script.js
@@ -158,39 +158,3 @@ function handleEnterTab(event) {
     }
 }
 
-//tela de login
-const loginForm = document.querySelector('#login-form');
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
-const errorMessage = document.querySelector('#error-message');
-
-// Senha e usuário
-const userCorreto = 'kerton';
-const senhaCorreta = '123';
-
-// evento para o envio do formulário
-loginForm.addEventListener('submit', function(event) {
-    // Impede o comportamento padrão do formulário de recarregar a página
-    event.preventDefault();
-
-    // Obtém os valores digitados pelo usuário
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-
-    // Limpa a mensagem de erro anterior
-    errorMessage.textContent = '';
-
-    // validação
-    if (username === userCorreto && password === senhaCorreta) {
-        // Se  estiverem corretas
-        alert('Login bem-sucedido! Bem-vindo(a), estagiário(a)!');
-        
-        // direciona para página
-        window.location.href = "vidaplus_paciente.html"; 
-        
-    } else {
-        // Se estiverem incorretas
-        errorMessage.textContent = 'Usuário ou senha inválidos. Tente novamente.';
-    }
-
-});
